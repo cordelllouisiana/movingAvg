ae53a6363998b209058402026b78d4018635e5a8
Cordell <vilienengineergomez@gmail.com>
2016-09-25 19:04:32 -0600

    Added function and implementation fo current_50_200 share prices

--- MovingDayAvgs.py
+++ MovingDayAvgs.py
@@ -7,5 +7,22 @@ stock are currently priced above their 200 day moving avg.
 """
 from yahoo_finance import Share
 
-yahoo = Share('YHOO')
-print (yahoo.get_200day_moving_avg())
+def determineAgainst50(curr,fifty):
+    if(curr > fifty):
+        decision = "buy"
+    elif(fifty >= curr):
+        decision = "sell"
+        
+    return decision
+    
+def main():
+    yahoo = Share('YHOO')
+    currPrice = yahoo.get_price()
+    fiftyDayMovingPrice = yahoo.get_50day_moving_avg()
+    twoHundredMovingPrice = yahoo.get_200day_moving_avg()
+    print("The current share price is: "+ yahoo.get_price())
+    print ("The current 50 Day Moving Avg is: " +yahoo.get_50day_moving_avg()+" "+ determineAgainst50(currPrice,fiftyDayMovingPrice))
+    print ("The current 200 Day Moving Avg is: " + yahoo.get_200day_moving_avg())
+
+if __name__ == "__main__":
+    main()
