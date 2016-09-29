4aec9b741dbc094472649f28ed221d26178dbd52
Cordell <vilienengineergomez@gmail.com>
2016-09-28 20:11:36 -0600

    Adding more logical and needed OOP

--- MovingDayAvgs.py
+++ MovingDayAvgs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Driver Function
 
 This is a script that searches the market and checks which
 stock are currently priced above their 200 day moving avg.
@@ -9,21 +9,18 @@ from yahoo_finance import Share
 import math
 from Decisions import *
 from Checks import *
+from ScreenOutput import *
     
 def main():
-    decisions = Decisions()
-    checks = Checks()
+    decisions = Decisions() #Creates empty instance of Decisions class
+    checks = Checks() #Creates empty instance of Checks Class
+    display = ScreenOutput()
     currentAmountToTrade = 1000 #$1000 US Dollars   will become a system arg
-    yahoo = Share('YHOO')
-    currPrice = yahoo.get_price()
+    yahoo = Share('YHOO') #Creates yahoo instance of Share type object
+    currPrice = yahoo.get_price() 
     fiftyDayMovingPrice = yahoo.get_50day_moving_avg()
     twoHundredMovingPrice = yahoo.get_200day_moving_avg()
-    print("Hello User, you have $"+str(currentAmountToTrade)+ " to trade today!") #In Future, this will current amount and check if you have enough
-    print("The current share price is: "+ yahoo.get_price())
-    print ("The current 50 Day Moving Avg is: " +yahoo.get_50day_moving_avg()+" "+ decisions.determineAgainst50(currPrice,fiftyDayMovingPrice))
-    print ("The current 200 Day Moving Avg is: " + yahoo.get_200day_moving_avg() +" "+ decisions.determineAgainst200(currPrice,twoHundredMovingPrice))
-    print ("You can purchase "+ str(checks.checkHowMany(currentAmountToTrade,currPrice)) + " shares of "+yahoo.symbol)
-    print (42.8042*23)
-
+    display.displayGreetings()
+    
 if __name__ == "__main__":
     main()
